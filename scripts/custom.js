6378bc97421c012a0cc73d4432c45dbbcb4df1df
Robo1993 <101870952+Robo1993@users.noreply.github.com>
2023-07-04 20:01:02 +0200

    added menus
    added the hamburger menu with drawer animation and buttons for the bottom bar menu.

--- scripts/custom.js
+++ scripts/custom.js
@@ -12,4 +12,37 @@ $(document).on('ready pjax:scriptcomplete',function(){
      * Code included inside this will only run once the page Document Object Model (DOM) is ready for JavaScript code to execute
      * @see https://learn.jquery.com/using-jquery-core/document-ready/
      */
+
+    $(".bottom-bar-item").on("click", function() {
+        $(".bottom-bar-item").each(function() {
+            $(this).css("background-color", "#fff");
+        });
+        $(this).css("background-color", "#ccc");
+    });
+
+    $("#hamburger-anchor-top").on("click", function() {
+        $(this).css("background-color", "#ccc");
+        openNav("#hamburger-drawer-top");
+        //$("#hamburger-drawer-top").addClass("drawer-left-animation");
+    });
+
+    $("#hamburger-anchor-bottom").on("click", function() {
+        $(this).css("background-color", "#ccc");
+        openNav("#hamburger-drawer-bottom");
+        //$("#hamburger-drawer-top").addClass("drawer-left-animation");
+    });
+
+    $("#mobile-content").on("click", function() {
+        closeNav("#hamburger-drawer-top");
+        closeNav("#hamburger-drawer-bottom");
+    });
 });
+
+
+function openNav(e) {
+  $(e).animate({width:"45%"});
+}
+
+function closeNav(e) {
+  $(e).animate({width:"0%"});
+}
